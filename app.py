cd0f81ac7259244d756faeca6fe5856cf8c6f8f8
shivaprasadyanala <shivaprasadyanala@gmail.com>
2022-12-01 10:52:29 +0530

    format

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask,render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash
 import mysql.connector
 import pyodbc
 # from flask_mysqldb import MySQL
-
 
 
 # app.config['MYSQL_HOST'] = 'localhost'
@@ -24,7 +23,8 @@ import pyodbc
 # installed mysql connection
 # conn = mysql.connector.connect(user='root', password='root', host='127.0.0.1', database='crud',port='3000')
 # xamp mysql
-conn = mysql.connector.connect(user='root', password='', host='127.0.0.1', database='flask')
+conn = mysql.connector.connect(
+    user='root', password='', host='127.0.0.1', database='flask')
 cur = conn.cursor()
 app = Flask(__name__)
 app.secret_key = 'secret key'
@@ -32,70 +32,77 @@ app.secret_key = 'secret key'
 
 @app.route('/')
 def home():
- cur = conn.cursor()
- cur.execute("SELECT * from student")
- data = cur.fetchall()
 
- # cur.close()
- # print(data)
- # return data;
- return render_template('index.html',students = data)
+    cur = conn.cursor()
+    cur.execute("SELECT * from student")
+    data = cur.fetchall()
 
-@app.route('/insert',methods=['POST','GET'])
+    # cur.close()
+    # print(data)
+    # return data;
+    return render_template('index.html', students=data)
+
+
+@app.route('/insert', methods=['POST', 'GET'])
 def insert():
- if request.method == "POST" or "GET":
-  
-  name = request.form['name'];
-  age = request.form['age'];
-  location = request.form['location']
-  cur = conn.cursor()
-  cur.execute('insert into student(name,age,location) values(%s,%s,%s)',(name,age,location));
-  conn.commit()
-  # cur.commit();
-  cur.close()
-  flash("data inserted successfully")
-  return redirect(url_for('home'))
+    if request.method == "POST" or "GET":
 
-@app.route('/edit/<string:id>',methods=['GET'])
+        name = request.form['name']
+        age = request.form['age']
+        location = request.form['location']
+        cur = conn.cursor()
+        cur.execute(
+            'insert into student(name,age,location) values(%s,%s,%s)', (name, age, location))
+        conn.commit()
+        # cur.commit();
+    cur.close()
+    flash("data inserted successfully")
+    return redirect(url_for('home'))
+
+
+@app.route('/edit/<string:id>', methods=['GET'])
 def edit(id):
-  cur = conn.cursor()
-  cur.execute("SELECT * from student where id=%s",(id,))
-  data = cur.fetchone()
+    cur = conn.cursor()
+    cur.execute("SELECT * from student where id=%s", (id,))
+    data = cur.fetchone()
 
-  # cur.close()
-  print(data)
-  # return "hlo";
-  return render_template('edit.html',students = data)
+    # cur.close()
+    print(data)
+    # return "hlo";
+    return render_template('edit.html', students=data)
 
-@app.route('/add',methods=['GET'])
+
+@app.route('/add', methods=['GET'])
 def add():
-  return render_template('add.html')
+    return render_template('add.html')
 
-@app.route('/delete/<string:id>',methods=['GET'])
+
+@app.route('/delete/<string:id>', methods=['GET'])
 def delete(id):
-  # id = request.body.id;
-  cur = conn.cursor()
-  cur.execute('delete from student where id = %s',(id,))
-  conn.commit()
-  cur.close()
-  flash("data deleted successfully")
-  return redirect(url_for('home'))
+    # id = request.body.id;
+    cur = conn.cursor()
+    cur.execute('delete from student where id = %s', (id,))
+    conn.commit()
+    cur.close()
+    flash("data deleted successfully")
+    return redirect(url_for('home'))
 
-@app.route('/update/<string:id>',methods=['POST','GET'])
+
+@app.route('/update/<string:id>', methods=['POST', 'GET'])
 def update(id):
- if request.method == 'POST':
-  print("hi")
-  name = request.form['name']
-  age = request.form['age']
-  location = request.form['location']
-  cur = conn.cursor()
-  cur.execute("update student set name=%s,age=%s,location=%s  where id = %s",(name,age,location,id))
-  conn.commit()
-  cur.close()
-  flash("data updated successfully")
-  return redirect(url_for('home'));
-
+    if request.method == 'POST':
+        print("hi")
+        name = request.form['name']
+        age = request.form['age']
+        location = request.form['location']
+        cur = conn.cursor()
+        cur.execute("update student set name=%s,age=%s,location=%s  where id = %s",
+                    (name, age, location, id))
+        conn.commit()
+        cur.close()
+        flash("data updated successfully")
+        return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
- app.run(debug = True)
+    app.run(debug=True)
